4b65d9e855214a6d52a9022c136135059bf1d9c0
Shubham Kori <shubhamkori23@gmail.com>
2022-07-04 15:49:10 +0530

    Update HelloController.java

--- Spring Boot/course-api/src/main/java/com/shubh/springbootstarter/controller/HelloController.java
+++ Spring Boot/course-api/src/main/java/com/shubh/springbootstarter/controller/HelloController.java
@@ -1,10 +1,14 @@
 package com.shubh.springbootstarter.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	@RequestMapping("/hello")
+	public String sayHi() {
+		return "Hi";
 	}
 
 }
