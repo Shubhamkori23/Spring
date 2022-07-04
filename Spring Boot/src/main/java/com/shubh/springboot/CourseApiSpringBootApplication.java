0e9225396fd77962cd530b15799b53721f4b55a3
Shubham Kori <shubhamkori23@gmail.com>
2022-07-04 14:34:53 +0530

    Update Application

--- Spring Boot/src/main/java/com/shubh/springboot/CourseApiSpringBootApplication.java
+++ Spring Boot/src/main/java/com/shubh/springboot/CourseApiSpringBootApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CourseApiSpringBootApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+
 		SpringApplication.run(CourseApiSpringBootApplication.class, args);
-	}
+    }
 
 }
--- Spring Boot/src/main/java/com/shubh/springbootstarter/CourseApiApp.java
+++
@@ -1,7 +0,0 @@
-package com.shubh.springbootstarter;
-
-public class CourseApiApp {
-    public static void main(String[] args) {
-        
-    }
-}
