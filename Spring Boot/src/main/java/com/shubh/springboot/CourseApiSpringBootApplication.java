909f8e6480247708768bb747515c546fb3e4dc67
Shubham Kori <shubhamkori23@gmail.com>
2022-07-04 13:17:38 +0530

    Create Spring Boot Application

---
+++ Spring Boot/src/main/java/com/shubh/springboot/CourseApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.shubh.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiSpringBootApplication.class, args);
+	}
+
+}
