5f32109dc99bc7b8d6d1af3c2ab765e9967c1163
Shubham Kori <shubhamkori23@gmail.com>
2022-07-06 11:29:54 +0530

    Update TopicService.java

--- Spring Boot/course-api-data/src/main/java/com/shubh/springbootstarter/topic/TopicService.java
+++ Spring Boot/course-api-data/src/main/java/com/shubh/springbootstarter/topic/TopicService.java
@@ -1,18 +1,25 @@
 package com.shubh.springbootstarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
+
+	@Autowired
+	private TopicRepository topicRepository;
 
 	private List<Topic> topics = Arrays.asList(new Topic("spring", "Spring Framework", "Spring Framework Description"),
 			new Topic("java", "Core Java", "Core Java Description"),
 			new Topic("javascript", "JavaScript", "JavaScript Description"));
 
 	public List<Topic> getAllTopics() {
+		List<Topic> topics = new ArrayList<>();
+		topicRepository.findAll().forEach(topics::add);
 		return topics;
 	}
 
@@ -21,7 +28,8 @@ public class TopicService {
 	}
 
 	public void addTopic(Topic topic) {
-		topics.add(topic);
+		topicRepository.save(topic);
+
 	}
 
 	public void updateTopic(String id, Topic topic) {
