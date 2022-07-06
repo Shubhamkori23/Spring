566f41fd1f9321b682db35ac9c3fb0df378aa9e0
Shubham Kori <shubhamkori23@gmail.com>
2022-07-06 10:34:53 +0530

    Update Topic.java

--- Spring Boot/course-api-data/src/main/java/com/shubh/springbootstarter/topic/Topic.java
+++ Spring Boot/course-api-data/src/main/java/com/shubh/springbootstarter/topic/Topic.java
@@ -1,13 +1,18 @@
 package com.shubh.springbootstarter.topic;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Topic {
 
+	@Id
 	private String id;
 	private String name;
 	private String description;
 
 	public Topic() {
-		super();
+
 	}
 
 	public Topic(String id, String name, String description) {
